7aff00a6dd3a3e576b2b3c0b52f492c74be48391
Kyle McCall <kylem147@gmail.com>
2022-08-04 16:20:47 -0700

    creates basic html and css setup and adds functional for input and button

--- app.js
+++ app.js
@@ -6,3 +6,32 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+import { renderGoblin } from './render-goblin.js';
+
+const formEl = document.querySelector('form');
+const goblinInput = document.getElementById('goblin-input');
+const goblinContainerEL = document.querySelector('.goblin-container');
+
+let defeatedGoblins = 0;
+let playerHp = 10;
+
+
+const goblinArr = [
+    {
+        name: 'Lorph',
+        hp: 3
+    },
+    {
+        name: 'Jeff',
+        hp: 3
+    },
+];
+
+formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const goblin = goblinInput.value;
+    
+
+})
