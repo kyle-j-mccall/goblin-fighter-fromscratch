d9b4f088e8be903dc788e922cd12e64fdf9f4f53
Kyle McCall <kylem147@gmail.com>
2022-08-05 11:28:59 -0700

    adds styling

--- render-goblin.js
+++ render-goblin.js
@@ -5,6 +5,21 @@ export function renderGoblin(name, hp) {
 
     goblinDiv.classList.add('goblin-list');
 
+    goblinNameEl.textContent = `${name} 😈`;
+    goblinHpEl.textContent = `HP: ${hp}`;
+
+    goblinDiv.append(goblinNameEl, goblinHpEl);
+
+    return goblinDiv;
+}
+
+export function renderDeadGoblin(name, hp) {
+    const goblinDiv = document.createElement('div');
+    const goblinNameEl = document.createElement('p');
+    const goblinHpEl = document.createElement('p');
+
+    goblinDiv.classList.add('goblin-list');
+
     goblinNameEl.textContent = name;
     goblinHpEl.textContent = hp;
 
